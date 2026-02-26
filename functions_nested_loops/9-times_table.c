6a8467c2cdfe3eb7dc38fbb622889e8ffb37783a
abdularhman2341 <abdulrhman3334@gmail.com>
2026-02-26 10:54:38 +0000

    Add times table function

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: Nothing
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int result;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			result = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result >= 10)
+				_putchar('0' + result / 10);
+			_putchar('0' + result % 10);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
