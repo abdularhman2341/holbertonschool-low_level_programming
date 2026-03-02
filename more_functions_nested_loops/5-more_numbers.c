2a0281d50d4ddc92c5de6f76158fb0ad0f4c3f7c
abdularhman2341 <abdulrhman3334@gmail.com>
2026-03-02 08:04:03 +0000

    Add more numbers function

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times numbers 0 to 14
+ *
+ * Return: Nothing
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (j >= 10)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
