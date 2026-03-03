2b65766ebf1b38eed565ebc1979e9040e3076c3f
abdularhman2341 <abdulrhman3334@gmail.com>
2026-03-03 10:37:31 +0000

    Fix infinite add betty style

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen_num - returns length of a string
+ * @s: pointer to the string
+ *
+ * Return: length
+ */
+int _strlen_num(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
  * infinite_add - adds two numbers stored as strings
  * @n1: first number
  * @n2: second number
@@ -11,51 +27,32 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1;
-	int len2;
-	int carry;
-	int sum;
-	int i;
+	int l1, l2, carry, sum, i, tmp;
 
-	len1 = 0;
-	while (n1[len1] != '\0')
-		len1++;
-	len2 = 0;
-	while (n2[len2] != '\0')
-		len2++;
+	l1 = _strlen_num(n1) - 1;
+	l2 = _strlen_num(n2) - 1;
 	carry = 0;
-	len1--;
-	len2--;
 	i = 0;
-	while (len1 >= 0 || len2 >= 0 || carry > 0)
+	while (l1 >= 0 || l2 >= 0 || carry > 0)
 	{
 		if (i >= size_r - 1)
 			return (0);
 		sum = carry;
-		if (len1 >= 0)
-		{
-			sum = sum + (n1[len1] - '0');
-			len1--;
-		}
-		if (len2 >= 0)
-		{
-			sum = sum + (n2[len2] - '0');
-			len2--;
-		}
+		if (l1 >= 0)
+			sum += (n1[l1--] - '0');
+		if (l2 >= 0)
+			sum += (n2[l2--] - '0');
 		carry = sum / 10;
-		r[i] = (sum % 10) + '0';
-		i++;
+		r[i++] = (sum % 10) + '0';
 	}
 	r[i] = '\0';
-	len1 = 0;
-	len2 = i - 1;
-	while (len1 < len2)
+	l1 = 0;
+	l2 = i - 1;
+	while (l1 < l2)
 	{
-		carry = r[len1];
-		r[len1] = r[len2];
-		r[len2] = carry;
-		len1++;
-		len2--;
+		tmp = r[l1];
+		r[l1++] = r[l2];
+		r[l2--] = tmp;
 	}
 	return (r);
 }
