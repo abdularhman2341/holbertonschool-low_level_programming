46da5dfb358cf07c647b82f6e78efeae230da6aa
abdularham2341 <abdulrhman3334@gmail.com>
2026-04-21 11:21:06 +0300

    Replace strlen with manual length counting in add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	unsigned int len = 0;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -24,7 +25,10 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new->len = strlen(str);
+	while (str[len] != '\0')
+		len++;
+
+	new->len = len;
 	new->next = *head;
 	*head = new;
 
