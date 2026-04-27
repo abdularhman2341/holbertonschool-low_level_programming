2a4604b62de18b0a39787124b364525b3de51218
abdularham2341 <abdulrhman3334@gmail.com>
2026-04-27 05:36:04 +0300

    fix betey

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -5,7 +5,7 @@
  * @ht: The hash table to look into
  * @key: The key you are looking for
  *
- * Return: The value associated with the element, or NULL if key couldn't be found
+ * Return: The value associated with the element, or NULL if not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
